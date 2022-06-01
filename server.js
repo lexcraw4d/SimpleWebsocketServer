0fed09846207ef1e3858fac1c34df8a759bd4b35
Lex Crawford <lexcraw4d@gmail.com>
2022-06-01 17:53:52 -0400

    simple echo socket set up

--- server.js
+++ server.js
@@ -5,4 +5,16 @@ const wsServer = new WebSocket.Server ({
     port: PORT
 });
 
+wsServer.on('connection', function(socket){
+    //Attach behavior to the incoming socket
+
+    socket.on('message', function(msg){
+        console.log(`Recieved msg from client: ${msg}` )
+        socket.send(`Take this back: ${msg}`)
+    })
+
+    // Some feedback to client
+    console.log('A client connected')
+})
+
 console.log(`${new Date()} server is listening on ${PORT}`);
